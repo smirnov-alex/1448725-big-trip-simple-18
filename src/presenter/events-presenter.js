85e0e9429b2d7208829ae75400f19b245eac2410
Alex <smirnov1991alexx@gmail.com>
2022-08-22 23:47:59 +0500

    Part 1. Revolution or evolution

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,34 +1,69 @@
 import EventsListView from '../view/events-list';
 import EditPointView from '../view/edit-point-view';
-import AddPointView from '../view/add-point-view';
+//import AddPointView from '../view/add-point-view';
 import PointView from '../view/point-view';
 import PointModel from '../model/point';
 import { render } from '../render.js';
 
 export default class EventsPresenter {
-  eventsList = new EventsListView();
-  init = (eventsContainer) => {
-    this.eventsContainer = eventsContainer;
-    this.pointModel = new PointModel();
-    this.points = [...this.pointModel.getPoints()];
-    render(this.eventsList, this.eventsContainer);
-    render(new EditPointView(
-      this.points[0],
-      this.pointModel.getPointOffers(this.points[0]),
-      this.pointModel.getPointDestination(this.points[0])),
-    this.eventsList.getElement());
-    render(new AddPointView(
-      this.points[1],
-      this.pointModel.getPointOffers(this.points[1]),
-      this.pointModel.getPointDestination(this.points[1])),
-    this.eventsList.getElement());
+  #eventsContainer = null;
+  #pointModel = null;
+  #eventsList = new EventsListView();
 
-    for (let i = 2; i < this.points.length; i++) {
-      render(new PointView(
-        this.points[i],
-        this.pointModel.getPointOffers(this.points[i]),
-        this.pointModel.getPointDestination(this.points[i])),
-      this.eventsList.getElement());
+  #points = [];
+  init = (eventsContainer) => {
+    this.#eventsContainer = eventsContainer;
+    this.#pointModel = new PointModel();
+    this.#points = [...this.#pointModel.points];
+    render(this.#eventsList, this.#eventsContainer);
+    /*
+    render(new AddPointView(
+      this.#points[1],
+      this.#pointModel.getPointOffers(this.#points[1]),
+      this.#pointModel.getPointDestination(this.#points[1])),
+    this.#eventsList.element);
+    */
+    for (let i = 2; i < this.#points.length; i++) {
+      this.#renderPoint(this.#points[i]);
     }
+
+  };
+
+  #renderPoint = (point) => {
+    const pointComponent = new PointView(
+      point,
+      this.#pointModel.getPointOffers(point),
+      this.#pointModel.getPointDestination(point));
+    const editPointComponent = new EditPointView(
+      point,
+      this.#pointModel.getPointOffers(point),
+      this.#pointModel.getPointDestination(point));
+    const replacePointToEdit = () => {
+      this.#eventsList.element.replaceChild(editPointComponent.element, pointComponent.element);
+    };
+    const replaceEditToPoint = () => {
+      this.#eventsList.element.replaceChild(pointComponent.element, editPointComponent.element);
+    };
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceEditToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+    pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replacePointToEdit();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+    editPointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceEditToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+    editPointComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceEditToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+    render(pointComponent, this.#eventsList.element);
   };
 }
--- src/view/add-point-view.js
+++ src/view/add-point-view.js
@@ -88,24 +88,28 @@ const createAddPointTemplate = (point, offers, destination) => {
 };
 
 export default class AddPointView {
+  #element = null;
+  #point = null;
+  #offers = null;
+  #destination = null;
   constructor(point, offers, destination) {
-    this.point = point;
-    this.offers = offers;
-    this.destination = destination;
+    this.#point = point;
+    this.#offers = offers;
+    this.#destination = destination;
   }
 
-  getTemplate() {
-    return createAddPointTemplate(this.point, this.offers, this.destination);
+  get template() {
+    return createAddPointTemplate(this.#point, this.#offers, this.#destination);
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
